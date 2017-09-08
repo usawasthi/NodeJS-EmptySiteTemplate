24b4109d0459e7dbc8f7615b88c439f577b7651f
usawasthi <31685431+usawasthi@users.noreply.github.com>
2017-09-08 15:08:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var http = require('http');
 var express = require('./node_modules/express');
 var app = express();
 
-app.get('/upload', function (req, res) {
-    res.send(
-    '<form action="/upload" method="post" enctype="multipart/form-data">' +
-    '<input type="file" name="snapshot" />' +
-    '<input type="submit" value="Upload" />' +
-    '</form>'
-    );
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+  res.write('<input type="file" name="filetoupload"><br>');
+  res.write('<input type="submit">');
+  res.write('</form>');
+  return res.end();
+}).listen(8080);
