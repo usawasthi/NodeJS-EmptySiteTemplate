60643b8da5da394b5c298a44460163f914eb80b5
usawasthi <31685431+usawasthi@users.noreply.github.com>
2017-09-08 13:17:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,7 @@ var app = express();
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world USA!');
     }).listen(process.env.PORT || 8080);
 
-app.get('/upload', function (req, res) {
-    res.send(
-    '<form action="/upload" method="post" enctype="multipart/form-data">' +
-    '<input type="file" name="snapshot" />' +
-    '<input type="submit" value="Upload" />' +
-    '</form>'
-    );
-});
-
-app.post('/upload', function (req, res) {
-    var path = req.files.snapshot.path;
-    var bs= azure.createBlobService();
-    bs.createBlockBlobFromFile('mycontainer', 'test.png', path, function (error) { });
-    res.send("OK");
-});
 
