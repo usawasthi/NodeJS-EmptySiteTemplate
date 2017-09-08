3cf288268a6533a37577929d4deb7a7e6c545388
usawasthi <31685431+usawasthi@users.noreply.github.com>
2017-09-08 11:59:19 +0530

    Update server.js
    Added section from Sarvana

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var http = require('http');
+var express = require('express');
+var formidable = require('formidable');
+var app = express();
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world USA!');
-    
-}).listen(process.env.PORT || 8080);
+app.get('/upload', function (req, res) {
+    res.send(
+    '<form action="/upload" method="post" enctype="multipart/form-data">' +
+    '<input type="file" name="snapshot" />' +
+    '<input type="submit" value="Upload" />' +
+    '</form>'
+    );
+});
+
+app.post('/upload', function (req, res) {
+    var path = req.files.snapshot.path;
+    var bs= azure.createBlobService();
+    bs.createBlockBlobFromFile('c', 'test.png', path, function (error) { });
+    res.send("OK");
+});
+
