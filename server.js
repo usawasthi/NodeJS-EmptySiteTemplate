d63a833938548fb5d193b21e66c5a9cb96255148
usawasthi <31685431+usawasthi@users.noreply.github.com>
2017-09-08 13:22:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ var http = require('http');
 var express = require('./node_modules/express');
 var app = express();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    }).listen(process.env.PORT || 8080);
-
 app.get('/upload', function (req, res) {
     res.send(
     '<form action="/upload" method="post" enctype="multipart/form-data">' +
