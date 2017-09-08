1c42561a6309fb843a3580f1019580a99b0c42ca
usawasthi <31685431+usawasthi@users.noreply.github.com>
2017-09-08 13:19:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ var app = express();
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world USA!');
     }).listen(process.env.PORT || 8080);
 
-
+app.get('/upload', function (req, res) {
+    res.send(
+    '<form action="/upload" method="post" enctype="multipart/form-data">' +
+    '<input type="file" name="snapshot" />' +
+    '<input type="submit" value="Upload" />' +
+    '</form>'
+    );
+});
